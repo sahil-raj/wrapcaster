db0b75afe5d62d39d4512b3e83665bb084561395
sahil-raj <sahilrajaaru@gmail.com>
2024-12-02 02:20:35 +0530

    chore: test

--- api/index.tsx
+++ api/index.tsx
@@ -4,13 +4,17 @@ import { devtools } from "frog/dev";
 import { handle } from "frog/next";
 import { serveStatic } from "frog/serve-static";
 import axios from "axios";
-
 import "dotenv/config";
+import { createClient } from "@supabase/supabase-js";
 
 // Uncomment to use Edge Runtime.
 // export const config = {
 //   runtime: 'edge',
 // }
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(`${supabaseUrl}`, `${supabaseKey}`);
 
 export const app = new Frog({
   assetsPath: "/",
@@ -20,6 +24,7 @@ export const app = new Frog({
   title: "Frog Frame",
 });
 
+//first frame
 app.frame("/", (c) => {
   return c.res({
     action: "/getWrapped",
@@ -58,7 +63,9 @@ app.frame("/", (c) => {
   });
 });
 
+//first non-cached route
 app.frame("/getWrapped", async (c) => {
+  //get name from fid for personalized response
   const headers = {
     Accept: "application/json",
     "API-KEY": `${process.env.WIELD_API_KEY}`,
@@ -68,8 +75,51 @@ app.frame("/getWrapped", async (c) => {
     `https://build.wield.xyz/farcaster/v2/user?fid=${c.frameData?.fid}`,
     { headers }
   );
-
   const uName = await res.data.result.user.displayName;
+
+  //check if the button is clicked for wrap data
+  const { buttonValue, status } = c;
+
+  let responseText: string = `Hi, ${uName} let's get  your wrap`;
+
+  if (status == "response" && buttonValue == "get_wrap") {
+    responseText = "Hey";
+  }
+  //if the button is clicked the frame is rerendered with buttonValue
+  if (status == "response" && buttonValue == "get_wrap") {
+    (async () => {
+      const { data, error } = await supabase
+        .from("wrapData")
+        .select("*")
+        .eq("fid", `${c.frameData?.fid}`);
+
+      if (!error) {
+        if (data.length > 0) {
+          //data found
+          //display the response
+          console.log("found");
+        } else {
+          //data not found
+          //get from wield pass it down to ML model
+
+          //get casts
+          const casts = await axios.get(
+            `https://build.wield.xyz/farcaster/v2/casts?fid=${c.frameData?.fid}`,
+            { headers }
+          );
+
+          /*description, following, followers, wallets*/
+          console.log(
+            "on the job",
+            await casts.data.result.casts[1].text,
+            res.data.result.user.followingCount,
+            res.data.result.user.followerCount,
+            res.data.result.user.allConnectedAddresses
+          );
+        }
+      }
+    })();
+  }
 
   return c.res({
     image: (
@@ -99,11 +149,16 @@ app.frame("/getWrapped", async (c) => {
             whiteSpace: "pre-wrap",
           }}
         >
-          {`Hi, ${uName} let's check your wrap!`}
+          {responseText}
         </div>
       </div>
     ),
+    intents: [<Button value="get_wrap">Give my wrap</Button>],
   });
+});
+
+app.hono.get("/test", (c) => {
+  return c.json("ribbit");
 });
 
 // @ts-ignore
