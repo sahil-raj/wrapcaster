86331375a481cc7b82a0248b3f32ea7f6399926d
sahil-raj <sahilrajaaru@gmail.com>
2024-12-01 20:49:23 +0530

    chore: basic frame routes and design

--- api/index.tsx
+++ api/index.tsx
@@ -1,8 +1,11 @@
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
+import { Button, Frog } from "frog";
+import { devtools } from "frog/dev";
 // import { neynar } from 'frog/hubs'
-import { handle } from 'frog/next'
-import { serveStatic } from 'frog/serve-static'
+import { handle } from "frog/next";
+import { serveStatic } from "frog/serve-static";
+import axios from "axios";
+
+import "dotenv/config";
 
 // Uncomment to use Edge Runtime.
 // export const config = {
@@ -10,67 +13,103 @@ import { serveStatic } from 'frog/serve-static'
 // }
 
 export const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
+  assetsPath: "/",
+  basePath: "/api",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-  title: 'Frog Frame',
-})
+  title: "Frog Frame",
+});
 
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.frame("/", (c) => {
   return c.res({
+    action: "/getWrapped",
     image: (
       <div
         style={{
-          alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
+          alignItems: "center",
+          background: "linear-gradient(to right, #432889, #17101F)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         <div
           style={{
-            color: 'white',
+            color: "white",
             fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
             lineHeight: 1.4,
             marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          {`Let's wrap it up?`}
         </div>
       </div>
     ),
-    intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
-    ],
-  })
-})
+    intents: [<Button action="/getWrapped">Get Wrapped</Button>],
+  });
+});
+
+app.frame("/getWrapped", async (c) => {
+  const headers = {
+    Accept: "application/json",
+    "API-KEY": `${process.env.WIELD_API_KEY}`,
+  };
+
+  const res = await axios.get(
+    `https://build.wield.xyz/farcaster/v2/user?fid=${c.frameData?.fid}`,
+    { headers }
+  );
+
+  const uName = await res.data.result.user.displayName;
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: "center",
+          background: "linear-gradient(to right, #432889, #17101F)",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        <div
+          style={{
+            color: "white",
+            fontSize: 60,
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
+          }}
+        >
+          {`Hi, ${uName} let's check your wrap!`}
+        </div>
+      </div>
+    ),
+  });
+});
 
 // @ts-ignore
-const isEdgeFunction = typeof EdgeFunction !== 'undefined'
-const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
-devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
